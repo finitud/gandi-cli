b9f6eb947e83d52d08fd6694690d3929a90f43d4
Alice Wyan <alice@finitud.org>
2016-02-27 18:31:32 +0000

    Use setuptools

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "gandi-cli",
+    version = "0.1.0",
+    author = "Alice Wyan",
+    author_email = "alice@finitud.org",
+    description = "A CLI tool for gandi.net",
+    license = "PSF",
+    keywords = "gandi cli gandi-cli",
+    url = "http://github.com/finitud/gandi-cli",
+    packages = find_packages(),
+
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        gandi=gandi:cli
+    ''',
+
+)
