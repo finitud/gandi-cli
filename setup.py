803e21b4539bec9f27b5cb27e7a4a95348edc167
Alice Wyan <alice@finitud.org>
2016-02-27 19:44:56 +0000

    Fix PEP8 formatting

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "gandi-cli",
-    version = "0.1.0",
-    author = "Alice Wyan",
-    author_email = "alice@finitud.org",
-    description = "A CLI tool for gandi.net",
-    license = "PSF",
-    keywords = "gandi cli gandi-cli",
-    url = "http://github.com/finitud/gandi-cli",
-    packages = find_packages(),
+    name="gandi-cli",
+    version="0.1.0",
+    author="Alice Wyan",
+    author_email="alice@finitud.org",
+    description="A CLI tool for gandi.net",
+    license="PSF",
+    keywords="gandi cli gandi-cli",
+    url="http://github.com/finitud/gandi-cli",
+    packages=find_packages(),
 
     install_requires=[
         'Click',
